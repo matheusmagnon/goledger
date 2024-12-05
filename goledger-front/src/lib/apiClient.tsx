f4b530885d41e65fcebacd94659b97ebc14f07ac
Matheus <matheusmagnon@gmail.com>
2024-12-05 12:43:24 -0300

    fix: url

--- goledger-front/src/lib/apiClient.tsx
+++ goledger-front/src/lib/apiClient.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://ec2-54-91-215-149.compute-1.amazonaws.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
